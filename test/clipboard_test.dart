879c55dee332afa2b9c46d884b72b43518c6b081
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-29 20:13:45 +0100

    added example directory

---
+++ test/clipboard_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:clipboard/clipboard.dart';
+
+void main() {
+  test('add string to copy method', () {
+    final clipboard = FlutterClipboard();
+  });
+}
